40c7fac544652e4c690245bfb4d45e41b1cd3868
sikongconglu <1209064513@qq.com>
2022-07-13 10:15:50 +0800

    退出登录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,29 +3,41 @@
     <router-link to="/home">
       <img src="@/assets/logo.png" alt="" style="height:80px;margin-left: 105px;" />
     </router-link>
-    <div v-if="Ifshow" class="loginregister-buttons">
+    <div v-if="Ifshow1" class="loginregister-buttons">
       <el-button type="primary" plain style="margin-right:15px;">注册</el-button>
       <router-link to="login">
         <el-button plain>登录</el-button>
       </router-link>
+    </div>
+    <div v-if="Ifshow2" class="loginregister-buttons">
+      <el-button @click="logout()" type="primary" plain style="margin-right:15px">退出登录</el-button>
     </div>
   </div>
 </template>
 
 
 <script lang="ts">
+import router from '@/router';
 import { inject } from 'vue'
 
 export default {
   name: "Header",
   setup() {
     const pageinfo: string = inject('pageinfo') || '';
-    let Ifshow = true;
+    let Ifshow1 = true;
+    let Ifshow2 = false;
     if (pageinfo == 'HomePage') {
-      Ifshow = false;
+      Ifshow1 = false;
+      Ifshow2 = true;
+    }
+    const logout = () =>{
+      localStorage.removeItem("token");
+      router.push("/");
     }
     return {
-      Ifshow
+      Ifshow1,
+      Ifshow2,
+      logout
     }
   },
 };
